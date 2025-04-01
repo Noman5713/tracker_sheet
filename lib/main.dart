122bb7cda6036c9e166f99ca52b00bcd526f5c0d
Noman5713 <noman15-5713@diu.edu.bd>
2025-04-01 16:40:24 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeActivity(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Problem 1: Hello World"),
+        backgroundColor: const Color.fromARGB(255, 184, 222, 241),
+      ),
+      body: Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            color: Colors.deepOrangeAccent,
+            fontFamily: 'DancingScript',
+            fontSize: 50,
+          ),
+        ),
+      ),
+    );
+  }
+}
