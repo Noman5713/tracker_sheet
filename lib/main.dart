cac81b975b89070bfc7b8fe5543d8649b79a4fc6
Noman5713 <noman15-5713@diu.edu.bd>
2025-04-02 03:21:55 +0600

    Problem 7: Grid View

--- lib/main.dart
+++ lib/main.dart
@@ -9,75 +9,241 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginScreen(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      home: GridViewScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  void _validateLogin() {
-    if (_formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("Login Successful!")));
-    }
-  }
-
+class GridViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text("Grid View"),
         backgroundColor: const Color.fromARGB(255, 184, 222, 241),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: "Username",
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter your username";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 15),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter your password";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(onPressed: _validateLogin, child: Text("Login")),
-            ],
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=25'),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=26'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=27'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=28'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=29'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=30'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=57'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=32'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=33'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=34'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=35'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=36'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=37'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=38'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=39'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=40'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=41'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=42'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=25'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=26'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=27'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=28'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=29'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=30'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=57'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=32'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=33'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=34'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=35'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=36'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=37'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=38'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=39'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=40'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=41'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=42'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=25'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=26'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=27'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=28'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=29'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=30'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=57'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=32'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=33'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=34'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=35'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=36'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=37'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=38'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=39'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=40'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=41'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image.network('https://picsum.photos/200?image=42'),
+          ),
+        ],
       ),
     );
   }
