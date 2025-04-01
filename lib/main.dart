b58efada02fd5876bc249e5836eef2ab3dc3f293
Noman5713 <noman15-5713@diu.edu.bd>
2025-04-01 22:12:01 +0600

    Problem 3: List View

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity(), debugShowCheckedModeBanner: false);
+    return MaterialApp(home: ItemList(), debugShowCheckedModeBanner: false);
   }
 }
 
-class HomeActivity extends StatefulWidget {
-  _HomeActivityState createState() => _HomeActivityState();
-}
-
-class _HomeActivityState extends State<HomeActivity> {
-  String text = "Press the Button";
-
-  void _afterclick() {
-    setState(() {
-      text = "Button Pressed";
-    });
-  }
-
+class ItemList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Problem 2: Button Pressed"),
+        title: Text("Problem 3: List View"),
         backgroundColor: const Color.fromARGB(255, 184, 222, 241),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(text, style: TextStyle(fontSize: 50)),
-            //Container(padding: EdgeInsets.all(20), color: Colors.blue),
-            ElevatedButton(
-              onPressed: _afterclick,
-              child: Text("Button", style: TextStyle(fontSize: 30)),
+      body: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 207, 219, 229),
+              borderRadius: BorderRadius.circular(10),
             ),
-          ],
-        ),
+            child: Text(
+              'Item ${index + 1}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          );
+        },
       ),
     );
   }
