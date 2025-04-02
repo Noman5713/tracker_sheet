712bd3fca0b5f4a1c8ecdac7bba85916f4345aba
Noman5713 <noman15-5713@diu.edu.bd>
2025-04-02 20:34:07 +0600

    Problem 12: Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GridViewScreen(),
+      home: _ContainerScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class GridViewScreen extends StatelessWidget {
+class _ContainerScreen extends StatelessWidget {
+  const _ContainerScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Local Image"),
+        title: Text("Container Screen"),
         backgroundColor: const Color.fromARGB(255, 184, 222, 241),
       ),
-      body: Center(child: Image.asset("assets/image/us.jpg")),
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 200,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 237, 237, 237),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
+          ),
+          child: Center(
+            child: Text(
+              "This is a text widget inside a container widget",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
